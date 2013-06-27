75a2b63039c2695dcd10c711ff73a22bcf47417e
Jonathan Ong <jonathanrichardong@gmail.com>
2013-06-27 15:07:10 -0700

    tests for resizing all the images

--- test/resize.js
+++ test/resize.js
@@ -35,7 +35,7 @@ describe('Resize', function () {
       identity.Interlace.should.not.equal('None')
     })
 
-    it('should have less than 85 quality', function () {
+    it('should have 85 quality', function () {
       parseInt(identity.Quality || identity['JPEG-Quality'], 10).should.equal(85)
     })
 
@@ -45,10 +45,90 @@ describe('Resize', function () {
   })
 
   describe('PNG', function () {
+    var file = path.join(__dirname, 'taylor-swift.png')
+    var identity
 
+    it('should work', function (done) {
+      service.resizeImage(fs.createReadStream(file), null, {
+        slug: 'a',
+        format: 'png'
+      }, function (err, location) {
+        if (err)
+          throw err
+
+        service.identify(location, function (err, _identity) {
+          if (err)
+            throw err
+
+          identity = _identity
+          done()
+        })
+      })
+    })
+
+    it('should be the correct size', function () {
+      var size = identity.size
+      size.width.should.equal(81)
+      size.height.should.equal(120)
+    })
+
+    it('should be progressive', function () {
+      identity.Interlace.should.not.equal('None')
+    })
   })
 
   describe('GIF', function () {
+    it('should throw', function () {
+      ;(function () {
+        service.resizeImage(fs.createReadStream(path.join(__dirname, 'crazy-laugh.gif')), null, {
+          slug: 'a',
+          format: 'gif'
+        }, function (err) {
+          if (err)
+            throw err
+        })
+      }).should.throw()
+    })
+  })
 
+  describe('PNG to JPG', function () {
+    var file = path.join(__dirname, 'taylor-swift.png')
+    var identity
+
+    it('should work', function (done) {
+      service.resizeImage(fs.createReadStream(file), null, {
+        slug: 'a',
+        format: 'jpg'
+      }, function (err, location) {
+        if (err)
+          throw err
+
+        service.identify(location, function (err, _identity) {
+          if (err)
+            throw err
+
+          identity = _identity
+          done()
+        })
+      })
+    })
+
+    it('should be the correct size', function () {
+      var size = identity.size
+      size.width.should.equal(81)
+      size.height.should.equal(120)
+    })
+
+    it('should be progressive', function () {
+      identity.Interlace.should.not.equal('None')
+    })
+
+    it('should have 85 quality', function () {
+      parseInt(identity.Quality || identity['JPEG-Quality'], 10).should.equal(85)
+    })
+
+    it('should be JPEG', function () {
+      identity.format.should.equal('JPEG')
+    })
   })
 })
