c221747f0bd99f32400cad75d36feffeb4b051ce
Jonathan Ong <jonathanrichardong@gmail.com>
2013-06-27 15:36:37 -0700

    put test works

---
+++ test/put.js
@@ -0,0 +1,70 @@
+var path = require('path')
+var fs = require('fs')
+var service = require('../')()
+
+describe('Put', function () {
+  describe('JPG', function () {
+    var file = path.join(__dirname, 'originalSideways.jpg')
+
+    it('should work', function (done) {
+      service.identifyImage(fs.createReadStream(file), {
+        name: 'originalSideways'
+      }, function (err, metadata) {
+        if (err)
+          throw err
+
+        metadata.path.should.be.ok
+        metadata.stats.should.be.ok
+        metadata.identity.should.be.ok
+
+        service.uploadImage(metadata, function (err) {
+          if (err)
+            throw err
+
+          metadata.filename.should.equal('originalSideways.jpg')
+
+          done()
+        })
+      })
+    })
+  })
+
+  describe('PNG', function () {
+    var file = path.join(__dirname, 'taylor-swift.png')
+
+    it('should work', function (done) {
+      service.identifyImage(fs.createReadStream(file), {
+        name: 'taylor-swift'
+      }, function (err, metadata) {
+        if (err)
+          throw err
+
+        metadata.path.should.be.ok
+        metadata.stats.should.be.ok
+        metadata.identity.should.be.ok
+
+        service.uploadImage(metadata, function (err) {
+          if (err)
+            throw err
+
+          metadata.filename.should.equal('taylor-swift.png')
+
+          done()
+        })
+      })
+    })
+  })
+
+  describe('GIF', function () {
+    var file = path.join(__dirname, 'crazy-laugh.gif')
+
+    it('should throw', function (done) {
+      service.identifyImage(fs.createReadStream(file), null, function (err) {
+        if (!err)
+          throw new Error()
+
+        done()
+      })
+    })
+  })
+})
